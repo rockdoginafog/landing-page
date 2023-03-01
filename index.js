1970ec0ddd141fc76011a869b6c023d3beb6a705
rockdoginafog <121097609+rockdoginafog@users.noreply.github.com>
2023-03-01 19:34:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const hamburgerBtn = document.querySelector(".hamburger")
-const white = document.querySelectorAll(".white")
 const navBar = document.querySelector(".nav-bar")
 
 hamburgerBtn.addEventListener("click", () =>{
